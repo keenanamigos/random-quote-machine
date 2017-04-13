cd446e074409358b426d5b276ab57a1cf15b7cdb
Keenan Johnson <keenan.johnson1@outlook.com>
2017-04-12 23:30:41 -0500

    Add object containing quotes and authors

---
+++ src/quote.js
@@ -0,0 +1,42 @@
+var quotesAndAuthors = {
+    group1: {
+        quote: "The best preparation for tomorrow is doing your best today.",
+        authorName: "H. Jackson Brown Jr."
+    },
+    group2: {
+        quote: "My mission in life is not merely to survive, but to thrive; and to do so with some passion, some compassion, some humor, and some style.",
+        authorName: "Maya Angelou"
+    },
+    group3: {
+        quote: "I can't change the direction of the wind, but I can adjust my sails to always reach my destination.",
+        authorName: "Jimmy Dean"
+    },
+    group4: {
+        quote: "Believe you can and you're halfway there.",
+        authorName: "Theodore Roosevelt"
+    },
+    group5: {
+        quote: "We must let go of the life we have planned, so as to accept the one that is waiting for us.",
+        authorName: "Joseph Campbell"
+    },
+    group6: {
+        quote: "Put your heart, your mind, and soul even into your smallest acts. This is the secret of success.",
+        authorName: "Swami Sivananda"
+    },
+    group7: {
+        quote: "Nothing is impossible, the word itself says \'i'm possible\'!",
+        authorName: "Audrey Hepburn"
+    },
+    group8: {
+        quote: "The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart.",
+        authorName: "Helen Keller"
+    },
+    group9: {
+        quote: "A hero is someone who has given his or her life to something bigger than oneself.",
+        authorName: "Joseph Campbell"
+    },
+    group10: {
+        quote: "Try to be a rainbow in someone's cloud.",
+        authorName: "Maya Angelou"
+    }
+};
