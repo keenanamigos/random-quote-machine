333981f8892cfe2c9fc52c72fdab02d7fc646117
Keenan Johnson <keenan.johnson1@outlook.com>
2017-10-09 22:22:16 -0500

    Update unit tests to use the Quote class
    Store the file path to dist/quote.js in a variable since it's being used multiple times now in the getRandomInteger unit test

--- test/quote_test.js
+++ test/quote_test.js
@@ -8,22 +8,15 @@ describe("Mocha", () => {
 });
 
 describe("getRandomInteger", () => {
-    const getRandomInteger = require("../dist/quote").getRandomInteger;
+    const quoteFilePath = "../dist/quote";
+    const getRandomInteger = require(quoteFilePath).getRandomInteger;
+    const Quote = require(quoteFilePath).Quote;
 
     it("should return an integer between 0 and the length of the given array", () => {
         const arrayOfQuotes = [
-            {
-                quote: "Some Quote 1",
-                author: "Some Author 1"
-            },
-            {
-                quote: "Some Quote 2",
-                author: "Some Author 2"
-            },
-            {
-                quote: "Some Quote 3",
-                author: "Some Author 3"
-            }
+            new Quote("Some Quote 1", "Some Author 1"),
+            new Quote("Some Quote 2", "Some Author 2"),
+            new Quote("Some Quote 3", "Some Author 3")
         ];
 
         let arrayOfIntegers = [];
