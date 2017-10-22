b30ed37bbfce3b6f6a87a50678b281bbd670c397
Keenan Johnson <keenan.johnson1@outlook.com>
2017-10-22 12:10:06 -0400

    Use createNewQuote function to populate the quotesAndAuthors array with Quote objects
    Use let instead of const for quotesAndAuthors array in anticipation of the createNewQuote function being called to push new Quote objects into the array from the UI

--- dist/quote.js
+++ dist/quote.js
@@ -1,4 +1,74 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
 "use strict";
+
 Object.defineProperty(exports, "__esModule", { value: true });
 var Quote = /** @class */ (function () {
     function Quote(args) {
@@ -8,17 +78,17 @@ var Quote = /** @class */ (function () {
 }());
 exports.Quote = Quote;
 var quotesAndAuthors = [
-    new Quote({ quote: "The best preparation for tomorrow is doing your best today.", author: "H. Jackson Brown Jr." }),
-    new Quote({ quote: "My mission in life is not merely to survive, but to thrive; and to do so with some passion, some compassion, some humor, and some style.", author: "Maya Angelou" }),
-    new Quote({ quote: "I can't change the direction of the wind, but I can adjust my sails to always reach my destination.", author: "Jimmy Dean" }),
-    new Quote({ quote: "Believe you can and you're halfway there.", author: "Theodore Roosevelt" }),
-    new Quote({ quote: "We must let go of the life we have planned, so as to accept the one that is waiting for us.", author: "Joseph Campbell" }),
-    new Quote({ quote: "Put your heart, your mind, and soul even into your smallest acts. This is the secret of success.", author: "Swami Sivananda" }),
-    new Quote({ author: "Audrey Hepburn", quote: "Nothing is impossible, the word itself says \'i'm possible\'!" }),
-    new Quote({ quote: "The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart.", author: "Helen Keller" }),
-    new Quote({ quote: "A hero is someone who has given his or her life to something bigger than oneself.", author: "Joseph Campbell" }),
-    new Quote({ author: "Maya Angelou", quote: "Try to be a rainbow in someone's cloud." }),
-    new Quote({ quote: "In the day, do the day's work." })
+    createNewQuote({ quote: "The best preparation for tomorrow is doing your best today.", author: "H. Jackson Brown Jr." }),
+    createNewQuote({ quote: "My mission in life is not merely to survive, but to thrive; and to do so with some passion, some compassion, some humor, and some style.", author: "Maya Angelou" }),
+    createNewQuote({ quote: "I can't change the direction of the wind, but I can adjust my sails to always reach my destination.", author: "Jimmy Dean" }),
+    createNewQuote({ quote: "Believe you can and you're halfway there.", author: "Theodore Roosevelt" }),
+    createNewQuote({ quote: "We must let go of the life we have planned, so as to accept the one that is waiting for us.", author: "Joseph Campbell" }),
+    createNewQuote({ quote: "Put your heart, your mind, and soul even into your smallest acts. This is the secret of success.", author: "Swami Sivananda" }),
+    createNewQuote({ author: "Audrey Hepburn", quote: "Nothing is impossible, the word itself says \'i'm possible\'!" }),
+    createNewQuote({ quote: "The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart.", author: "Helen Keller" }),
+    createNewQuote({ quote: "A hero is someone who has given his or her life to something bigger than oneself.", author: "Joseph Campbell" }),
+    createNewQuote({ author: "Maya Angelou", quote: "Try to be a rainbow in someone's cloud." }),
+    createNewQuote({ quote: "In the day, do the day's work." })
 ];
 function createNewQuote(object) {
     if (object.hasOwnProperty("quote")) {
@@ -40,19 +110,24 @@ function getRandomQuote(array) {
     return array[index];
 }
 exports.getRandomQuote = getRandomQuote;
-// export function setQuoteObjectHTML(): void {
-//     let quoteParagraph = document.getElementById("quote");
-//     let quoteAuthor = document.getElementById("author");
-//     const quote = getRandomQuote(quotesAndAuthors);
-//     quoteParagraph.innerHTML = `"${quote.quote}" `;
-//     if (quote.author === undefined) {
-//         quote.author = "Unattributed";
-//     }
-//     quoteAuthor.innerHTML = `- ${quote.author}`;
-// }
-// // Set initial Quote
-// setQuoteObjectHTML();
-// const newQuoteButton = document.getElementById("newQuote");
-// newQuoteButton.onclick = () => {
-//     setQuoteObjectHTML();
-// } 
+function setQuoteObjectHTML() {
+    var quoteParagraph = document.getElementById("quote");
+    var quoteAuthor = document.getElementById("author");
+    var quote = getRandomQuote(quotesAndAuthors);
+    quoteParagraph.innerHTML = "\"" + quote.quote + "\" ";
+    if (quote.author === undefined) {
+        quote.author = "Unattributed";
+    }
+    quoteAuthor.innerHTML = "- " + quote.author;
+}
+exports.setQuoteObjectHTML = setQuoteObjectHTML;
+// Set initial Quote
+setQuoteObjectHTML();
+var newQuoteButton = document.getElementById("newQuote");
+newQuoteButton.onclick = function () {
+    setQuoteObjectHTML();
+};
+
+
+/***/ })
+/******/ ]);
--- src/quote.ts
+++ src/quote.ts
@@ -10,19 +10,18 @@ export class Quote {
     }
  }
 
-const quotesAndAuthors = [
-    
-    new Quote({quote: "The best preparation for tomorrow is doing your best today.", author: "H. Jackson Brown Jr."}),
-    new Quote({quote: "My mission in life is not merely to survive, but to thrive; and to do so with some passion, some compassion, some humor, and some style.", author: "Maya Angelou"}),
-    new Quote({quote: "I can't change the direction of the wind, but I can adjust my sails to always reach my destination.", author: "Jimmy Dean"}),
-    new Quote({quote: "Believe you can and you're halfway there.", author: "Theodore Roosevelt"}),
-    new Quote({quote: "We must let go of the life we have planned, so as to accept the one that is waiting for us.", author: "Joseph Campbell"}),
-    new Quote({quote: "Put your heart, your mind, and soul even into your smallest acts. This is the secret of success.", author: "Swami Sivananda"}),
-    new Quote({author: "Audrey Hepburn", quote: "Nothing is impossible, the word itself says \'i'm possible\'!"}),
-    new Quote({quote: "The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart.", author: "Helen Keller"}),
-    new Quote({quote: "A hero is someone who has given his or her life to something bigger than oneself.", author: "Joseph Campbell"}),
-    new Quote({author: "Maya Angelou", quote: "Try to be a rainbow in someone's cloud."}),
-    new Quote({quote: "In the day, do the day's work."})
+let quotesAndAuthors = [
+    createNewQuote({quote: "The best preparation for tomorrow is doing your best today.", author: "H. Jackson Brown Jr."}),
+    createNewQuote({quote: "My mission in life is not merely to survive, but to thrive; and to do so with some passion, some compassion, some humor, and some style.", author: "Maya Angelou"}),
+    createNewQuote({quote: "I can't change the direction of the wind, but I can adjust my sails to always reach my destination.", author: "Jimmy Dean"}),
+    createNewQuote({quote: "Believe you can and you're halfway there.", author: "Theodore Roosevelt"}),
+    createNewQuote({quote: "We must let go of the life we have planned, so as to accept the one that is waiting for us.", author: "Joseph Campbell"}),
+    createNewQuote({quote: "Put your heart, your mind, and soul even into your smallest acts. This is the secret of success.", author: "Swami Sivananda"}),
+    createNewQuote({author: "Audrey Hepburn", quote: "Nothing is impossible, the word itself says \'i'm possible\'!"}),
+    createNewQuote({quote: "The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart.", author: "Helen Keller"}),
+    createNewQuote({quote: "A hero is someone who has given his or her life to something bigger than oneself.", author: "Joseph Campbell"}),
+    createNewQuote({author: "Maya Angelou", quote: "Try to be a rainbow in someone's cloud."}),
+    createNewQuote({quote: "In the day, do the day's work."})
 ];
 
 export function createNewQuote(object: {quote: string, author?: string}): Quote {
@@ -45,25 +44,25 @@ export function getRandomQuote(array: Array<Quote>): Quote {
     return array[index];
 }
 
-// export function setQuoteObjectHTML(): void {
-//     let quoteParagraph = document.getElementById("quote");
-//     let quoteAuthor = document.getElementById("author");
+export function setQuoteObjectHTML(): void {
+    let quoteParagraph = document.getElementById("quote");
+    let quoteAuthor = document.getElementById("author");
 
-//     const quote = getRandomQuote(quotesAndAuthors);
+    const quote = getRandomQuote(quotesAndAuthors);
     
-//     quoteParagraph.innerHTML = `"${quote.quote}" `;
+    quoteParagraph.innerHTML = `"${quote.quote}" `;
 
-//     if (quote.author === undefined) {
-//         quote.author = "Unattributed";
-//     }
+    if (quote.author === undefined) {
+        quote.author = "Unattributed";
+    }
     
-//     quoteAuthor.innerHTML = `- ${quote.author}`;
-// }
+    quoteAuthor.innerHTML = `- ${quote.author}`;
+}
 
-// // Set initial Quote
-// setQuoteObjectHTML();
+// Set initial Quote
+setQuoteObjectHTML();
 
-// const newQuoteButton = document.getElementById("newQuote");
-// newQuoteButton.onclick = () => {
-//     setQuoteObjectHTML();
-// }
+const newQuoteButton = document.getElementById("newQuote");
+newQuoteButton.onclick = () => {
+    setQuoteObjectHTML();
+}
