752584923be42dc71e80227101be420b6c0c2514
Keenan Johnson <keenan.johnson1@outlook.com>
2017-12-19 20:09:43 -0600

    Update gulpfile to include Sass commands

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,29 @@
-const gulp = require('gulp');
-const typescript = require('gulp-typescript');
+const gulp = require("gulp");
+const typescript = require("gulp-typescript");
+const sass = require("gulp-sass");
 
 // pull in the project TypeScript config
-const typescriptConfig = typescript.createProject('tsconfig.json');
+const typescriptConfig = typescript.createProject("tsconfig.json");
 
 // Place compiled files in the dist directory
-gulp.task('scripts', () => {
+gulp.task("scripts", () => {
   const typescriptResult = typescriptConfig.src()
   .pipe(typescriptConfig());
-  return typescriptResult.js.pipe(gulp.dest('dist'));
+
+  return typescriptResult.js.pipe(gulp.dest("dist"));
+});
+
+gulp.task("styles", () => {
+  const sassResult = gulp.src("sass/**/*.scss")
+  .pipe(sass().on("error"), sass.logError);
+
+  return sassResult.pipe(gulp.dest("./css/"));
 });
 
 // Watch .ts files for changes 
-gulp.task('watch', ['scripts'], () => {
-  gulp.watch('src/*.ts', ['scripts']);
+gulp.task("watch", ["scripts", "styles"], () => {
+  gulp.watch("src/*.ts", ["scripts"]);
+  gulp.watch("sass/*.scss", ["styles"]);
 });
 
-gulp.task('default', ['watch']);
+gulp.task("default", ["watch"]);
